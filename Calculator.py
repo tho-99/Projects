a160b531f6896494698e83a995ea1981a66241bd
= <th2263@bath.ac.uk>
2025-09-28 13:19:33 +0100

    28-09-25

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+# Enable the user to input into the command line
+print("Please enter your desired calculation")
+calc = input()
+user_input = calc.split()
+
+# Remind user to use space separation in their calculation
+
+
+# List of methods a user can call within the calculator
+
+
